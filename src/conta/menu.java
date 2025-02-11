93413597738fa75ff1185e0216ae9f404c99bb5f
Willa Evangelista <willasenna89@gmail.com>
2025-02-11 12:45:43 -0300

    Primeiro comit, enviando projeto ao GitHub

--- src/conta/menu.java
+++ src/conta/menu.java
@@ -3,7 +3,7 @@ package conta;
 public class menu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Menu
 
 	}
 
--- src/conta/util/cores.java
+++ src/conta/util/cores.java
@@ -1,9 +1,9 @@
 package conta.util;
 
-public class cores {
+public class Cores {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Cores
 
 	}
 
